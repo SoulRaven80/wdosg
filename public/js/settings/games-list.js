85d7d3ae251004b918763eb057a8ab5478f06c6e
Leonardo Putz <leo.utn@gmail.com>
2025-02-21 16:14:50 +0100

    [bug] having an apostrophe in the game title causes attachment to not work #34

--- public/js/settings/games-list.js
+++ public/js/settings/games-list.js
@@ -26,7 +26,7 @@ $("#gamesListLink").on("click", function() {
                     '  <td>',
                     '    <div class="d-flex justify-content-end">',
                     `      <button type="button" class="btn bi-pencil" aria-label="Game info" alt="Game info" title="Game info" onclick="openEditModal('${game.id}')"></button>`,
-                    `      <button type="button" class="btn bi-paperclip" aria-label="Attachments" alt="Attachments" title="Attachments" onclick="openAttachModal('${game.id}', '${game.path}', '${game.name}')"></button>`,
+                    `      <button type="button" class="btn bi-paperclip" aria-label="Attachments" alt="Attachments" title="Attachments" onclick="openAttachModal('${game.id}', '${game.path}', '${game.name.replace(/'/g, "\\'")}')"></button>`,
                     `      <button type="button" class="btn bi-gear" aria-label="Dosbox config" alt="Dosbox config" title="Dosbox config" onclick="openDosboxConfigModal('${game.id}', '${game.path}')"></button>`,
                     `      <button type="button" class="btn bi-trash" aria-label="Delete" alt="Delete" title="Delete" onclick="openDeleteGameConfirmation('${game.id}')"></button>`,
                     '    </div>',
